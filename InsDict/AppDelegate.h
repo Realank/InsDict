9d5ce8e2df5dbabda4315d293d2adbb9e0b9a6cd
Realank <realank@126.com>
2017-05-09 08:51:43 +0800

    init project

---
+++ InsDict/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InsDict
+//
+//  Created by Realank on 2017/5/9.
+//  Copyright © 2017年 Realank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InsDict/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InsDict
+//
+//  Created by Realank on 2017/5/9.
+//  Copyright © 2017年 Realank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
